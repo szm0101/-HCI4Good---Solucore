b6be07bcb19ef0a865b45ceb107fbcdb5f7e4ec9
Jesse <jesse@Jesses-MacBook-Pro.local>
2023-07-20 20:03:18 -0300

    wenxu add Sidebar function.

--- src/actions/components/Sidebar/Sidebar.js
+++ src/actions/components/Sidebar/Sidebar.js
@@ -1,7 +1,16 @@
-<div class="sidebar">
-  <ul>
-    <li><a href="#">Home</a></li>
-    <li><a href="#">Building</a></li>
-    <li><a href="#">Setting</a></li>
-  </ul>
-</div>
+import React from 'react';
+
+
+function Sidebar() {
+  return(
+    <div class="sidebar">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Building</a></li>
+        <li><a href="#">Setting</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
