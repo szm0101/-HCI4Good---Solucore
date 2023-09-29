6c2b7cd717a7cee3db5ba327efaef7b0f47a3716
eltazy <mh70000@dal.ca>
2023-09-28 23:21:42 -0300

    Initial commit. Created login page and made it main page that loads up first. Mostly frontend imitation of Old website.

--- src/actions/components/navbar/Navbar.js
+++ src/actions/components/navbar/Navbar.js
@@ -5,16 +5,22 @@ import Logo from '../../assets/Solucore_Icon.png';
 
 function Navbar() {
     const navigate = useNavigate();
+    const handleSignOut = () => {
+        // Navigate to the root path and then refresh the page
+        navigate('/');
+        window.location.reload();
+    };
     return (
         <nav>
             <div className="navbar-container">
                 <img src={Logo} alt="logo" width={105} height={85}/>
                 <ul className="navbar-links">
-                    <li><a onClick={() => {navigate('/')}}>HOME</a></li>
+                    <li><a onClick={() => {navigate('/Home')}}>HOME</a></li>
                     <li><a onClick={() => {navigate('/Buildings')}}>BUILDINGS</a></li>
                     <li><a onClick={() => {navigate('/Alerts')}}>ALERTS</a></li>
                     <li><a onClick={() => {navigate('/Settings')}}>SETTINGS</a></li>
                     <li><a onClick={() => {navigate('/Report')}}>REPORT</a></li>
+                    <li><a onClick={() => {handleSignOut()}} style={{color:"red"}} >SIGN OUT</a></li>
                 </ul>
             </div>
         </nav>
