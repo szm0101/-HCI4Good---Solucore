1899b53b39261acd20e054e1afc0fefc2001c558
Tahseen Shanteer <th362212@dal.ca>
2023-09-30 00:03:12 -0300

    Added home, building, settings and notif icons and adjusted borders to match old website

--- src/actions/components/Sidebar/Sidebar.js
+++ src/actions/components/Sidebar/Sidebar.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import "./Sidebar.css"
 import {Link} from "react-router-dom";
+import HomeIcon from "../../assets/home-icon.png";
+import BuildingIcon from "../../assets/building-icon.png"
+import SettingIcon from "../../assets/settings-icon.png"
 
 function Sidebar() {
   return(
     <div class="sidebar">
       <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/buildings">Buildings</Link></li>
+          <li><Link to="/"><img src={HomeIcon} alt='Home Icon'/></Link></li>
+          <li><Link to="/buildings"><img src={BuildingIcon} alt='Building Icon'/></Link></li>
+          <li><Link to="/settings"><img src={SettingIcon} alt='Settings Icon'/></Link></li>
           <li><Link to="/Alerts">Alerts</Link></li>
-          <li><Link to="/settings">Settings</Link></li>
           <li><Link to="/Report">Report</Link></li>
       </ul>
     </div>
