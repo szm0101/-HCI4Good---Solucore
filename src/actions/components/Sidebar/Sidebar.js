4d54df7aa8f116d024d6d6a8557a2cef8c17fb17
Tahseen Shanteer <th362212@dal.ca>
2023-09-30 05:03:12 -0300

    Set active sidebar icons to max opacity when clicked

--- src/actions/components/Sidebar/Sidebar.js
+++ src/actions/components/Sidebar/Sidebar.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./Sidebar.css"
 import {Link} from "react-router-dom";
 import HomeIcon from "../../assets/home-icon.png";
-import BuildingIcon from "../../assets/building-icon.png"
-import SettingIcon from "../../assets/settings-icon.png"
+import BuildingIcon from "../../assets/building-icon.png";
+import SettingIcon from "../../assets/settings-icon.png";
+
 
 function Sidebar() {
+  const [activeIcon, setActiveIcon] = useState('Home');
+
+  const toggleOpacity = (name) => {
+    setActiveIcon(name);
+  }
+
   return(
-    <div class="sidebar">
+    <div className="sidebar">
       <ul>
-          <li><Link to="/"><img src={HomeIcon} alt='Home Icon'/></Link></li>
-          <li><Link to="/buildings"><img src={BuildingIcon} alt='Building Icon'/></Link></li>
-          <li><Link to="/settings"><img src={SettingIcon} alt='Settings Icon'/></Link></li>
+          <li><Link to="/"><img src={HomeIcon} alt='Home Icon' onClick={() => toggleOpacity('Home')} className={activeIcon === 'Home' ? 'active' : null} /></Link></li>
+          <li><Link to="/buildings"><img src={BuildingIcon} alt='Building Icon' onClick={() => toggleOpacity('Building')} className={activeIcon === 'Building' ? 'active' : null} /></Link></li>
+          <li><Link to="/settings"><img src={SettingIcon} alt='Settings Icon' onClick={() => toggleOpacity('Settings')} className={activeIcon === 'Settings' ? 'active' : null} /></Link></li>
           <li><Link to="/Alerts">Alerts</Link></li>
           <li><Link to="/Report">Report</Link></li>
       </ul>
