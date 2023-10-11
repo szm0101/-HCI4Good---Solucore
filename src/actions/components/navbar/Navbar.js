0e47980d2c67194e492ba70f06462370852b575f
Zhiming Shang <zh352715@dal.ca>
2023-10-11 13:35:36 -0300

    better look of import section

--- src/actions/components/navbar/Navbar.js
+++ src/actions/components/navbar/Navbar.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import {Nav, Navbar, NavDropdown} from 'react-bootstrap';
 import Logo from '../../assets/solutrak-logo.png';
 import NotificationIcon from "../../assets/notification-icon.png";
 
@@ -9,18 +7,18 @@ import NotificationIcon from "../../assets/notification-icon.png";
 function Navbars() {
     return (
         <Navbar expand="lg" data-bs-theme="dark">
-        <div className="navbar-container">
-        <Navbar.Brand href="/"><img src={Logo} alt="logo" className='solutrak-logo' /></Navbar.Brand>
-            
-            <Nav className="navbar-links">
-                <Nav.Item>
-                    <Nav.Link className="navbar-links" href="/Alerts">
-                        <img src={NotificationIcon} alt='Notification icon' className='notif-icon' />
-                    </Nav.Link>
-                </Nav.Item>
-            </Nav>
-        </div>
-    </Navbar>
+            <div className="navbar-container">
+                <Navbar.Brand href="/"><img src={Logo} alt="logo" className='solutrak-logo' /></Navbar.Brand>
+                
+                <Nav className="navbar-links">
+                    <Nav.Item>
+                        <Nav.Link className="navbar-links" href="/Alerts">
+                            <img src={NotificationIcon} alt='Notification icon' className='notif-icon' />
+                        </Nav.Link>
+                    </Nav.Item>
+                </Nav>
+            </div>
+        </Navbar>
     );
 }
 
