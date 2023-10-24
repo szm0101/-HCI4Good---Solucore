5b8c23134cd22e3a2381849b8a0d1d8f00544cc8
Zhiming Shang <zh352715@dal.ca>
2023-10-24 10:17:26 -0300

    right aligned navbar

--- src/actions/components/navbar/Navbar.js
+++ src/actions/components/navbar/Navbar.js
@@ -6,7 +6,7 @@ import Logo from '../../assets/solutrak-logo.png';
 import Photo from '../../assets/profile-icon.png';
 import NotificationIcon from "../../assets/notification-icon.png";
 import EditInfoIcon from "../../assets/edit-icon.png";
-import ChangePassIcon from "../../assets/changePass-icon.png";
+import ChangePassIcon from "../../assets/lock-icon.png";
 import SignOutIcon from "../../assets/signOut-icon.png";
 import { useCookies } from 'react-cookie'; //
 
@@ -27,15 +27,15 @@ function Navbars() {
                 <Navbar.Brand href="/Home"><img src={Logo} alt="logo" className='solutrak-logo' /></Navbar.Brand>
 
                 <Nav className="ml-auto">
-                    <Nav.Item className='me-3'>
+                    <Nav.Item className='me-4'>
                         <Nav.Link className="navbar-links" href="/Alerts">
                             <img src={NotificationIcon} alt='Notification icon' className='notif-icon' />
                         </Nav.Link>
                     </Nav.Item>
 
-                    <Nav.Item className='me-5'>
-                        <Nav className="me-5">
-                            <NavDropdown title={<img src={Photo} alt="profile photo" className='profile-photo' />} id="basic-nav-dropdown">
+                    <Nav.Item className='me-4'>
+                        <Nav>
+                            <NavDropdown align='end' title={<img src={Photo} alt="profile photo" className='profile-photo' />} className='me-3' id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#action/3.1">
                                     <img src={EditInfoIcon} alt='Edit info icon' className='dropdown-icon me-3' />
                                     Edit info
@@ -49,7 +49,7 @@ function Navbars() {
                                 <NavDropdown.Divider />
                                 
                                 <NavDropdown.Item className="text-danger" onClick={handleSignOut}>
-                                    <img src={SignOutIcon} alt='Sign out icon' className='dropdown-icon me-3 ms-1' />
+                                    <img src={SignOutIcon} alt='Sign out icon' className='dropdown-icon me-3' />
                                     Sign out
                                 </NavDropdown.Item>
                             </NavDropdown>
