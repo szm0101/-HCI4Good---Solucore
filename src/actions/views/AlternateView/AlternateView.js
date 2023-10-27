39f63677a1f7347f7eaadd5092a446b4ae7ea1be
Tahseen Shanteer <th362212@dal.ca>
2023-10-26 22:48:24 -0300

    added individual expandable table row toggling

--- src/actions/views/AlternateView/AlternateView.js
+++ src/actions/views/AlternateView/AlternateView.js
@@ -9,10 +9,10 @@ import { useState } from "react";
 import TrafficInfo from "./TrafficInfo";
 
 function AlternateView() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(Array(5).fill(false));
 
-  const toggle = () => {
-    setIsOpen(!isOpen);
+  const toggle = (index) => {
+    setIsOpen(prevIsOpen => prevIsOpen.map((item, i) => i === index ? !item : item))
   };
   return (
     <div className="wrapper">
@@ -28,54 +28,54 @@ function AlternateView() {
             <tr>
               <td>Building wheels</td>
             </tr>
-            <tr className="data-tables" onClick={() => toggle()}>
+            <tr className="data-tables" onClick={() => toggle(0)}>
               <td>General Operating Data</td>
             </tr>
-            {isOpen ? (
+            {isOpen[0] ? (
               <tr>
                 <table className="general-op-data">
-                  <tbody>{isOpen && <GeneralOpData />}</tbody>
+                  <tbody>{<GeneralOpData />}</tbody>
                 </table>
               </tr>
             ) : null}
 
-            <tr className="data-tables">
+            <tr className="data-tables" onClick={() => toggle(1)}>
               <td>Performance Data</td>
             </tr>
-            {isOpen ? (
+            {isOpen[1] ? (
               <tr>
                 <table className="general-op-data">
-                  <tbody>{isOpen && <PerformanceData />}</tbody>
+                  <tbody>{<PerformanceData />}</tbody>
                 </table>
               </tr>
             ) : null}
-            <tr className="data-tables">
+            <tr className="data-tables" onClick={() => toggle(2)}>
               <td>Maintenance information</td>
             </tr>
-            {isOpen ? (
+            {isOpen[2] ? (
               <tr>
                 <table className="general-op-data">
-                  <tbody>{isOpen && <MaintenanceInfo />}</tbody>
+                  <tbody>{<MaintenanceInfo />}</tbody>
                 </table>
               </tr>
             ) : null}
-            <tr className="data-tables">
+            <tr className="data-tables" onClick={() => toggle(3)}>
               <td>Fault information</td>
             </tr>
-            {isOpen ? (
+            {isOpen[3] ? (
               <tr>
                 <table className="general-op-data">
-                  <tbody>{isOpen && <FaultInfo />}</tbody>
+                  <tbody>{<FaultInfo />}</tbody>
                 </table>
               </tr>
             ) : null}
-            <tr className="data-tables">
+            <tr className="data-tables" onClick={() => toggle(4)}>
               <td>Traffic information</td>
             </tr>
-            {isOpen ? (
+            {isOpen[4] ? (
               <tr>
                 <table className="general-op-data">
-                  <tbody>{isOpen && <TrafficInfo />}</tbody>
+                  <tbody>{<TrafficInfo />}</tbody>
                 </table>
               </tr>
             ) : null}
