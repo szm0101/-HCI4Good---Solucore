2b83371b2df416d5e28809aa61f43c0fc272d94b
eliza <eliza.fraser@dal.ca>
2023-10-27 12:29:18 -0300

    add building info and devices clicking

---
+++ src/actions/views/AlternateView/BuildingInfo.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Data.css';
+
+function BuildingInfo() {
+  return (
+    <body>
+        <div class="buildingInfo">
+            <h4> Building Name</h4>
+            <p> # of devices - company name</p>
+        </div>
+    </body>
+  );
+}
+
+export default BuildingInfo;
---
+++ src/actions/views/AlternateView/Devices.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Data.css';
+
+function Devices() {
+  return (
+    <body>
+        <div class="devices">
+            <h4>Need to add tables</h4>
+        </div>
+    </body>
+  );
+}
+
+export default Devices;
