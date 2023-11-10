226881c74ebc2a2f86430d610df759466c7bcb76
eltazy <mh70000@dal.ca>
2023-11-10 08:31:38 -0400

    Initial Changes. Updated Forgot Page to look like old website. Did some fixes on Login page.(BUG: forms are unresponsive)

--- src/actions/views/Forgot/Forgot.js
+++ src/actions/views/Forgot/Forgot.js
@@ -1,12 +1,15 @@
 // Forgot.js
 import React, { useState } from 'react';
-import { Alert, Form, Button, Card } from 'react-bootstrap';
+import { Alert, Form, Button, Card, Image, Container } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
+import Logo from '../../assets/solutrak-logo.png';
+import './Forgot.css';
+
 
 const Forgot = () => {
-    const [username, setUsername] = useState('');
-    const [newPassword, setNewPassword] = useState('');
+    const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
+    const [error, setError] = useState('');
     const navigate = useNavigate();
 
     const handlePasswordReset = () => {
@@ -19,35 +22,29 @@ const Forgot = () => {
     };
 
     return (
-        <div style={{ backgroundColor: '#2b2d3c', minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-            <Card className="forgot-card px-5 py-5" style={{ background: '#2b2d3c', border: '1px solid rgba(85, 87, 99, 1)' }}>
-                <Card.Header className="text-white h3" style={{ background: 'transparent', borderColor: 'transparent' }}>Reset Password</Card.Header>
-                <Card.Body>
-                    <Form className="forgot-form">
+        <Container fluid className="forgot-container d-flex align-items-center justify-content-center">
+            <Card className="forgot-card w-25 h-50 px-0 py-0">
+                <div className="forgot-title-container text-center">
+                    <Image className="logo-big" src={Logo} alt="Logo" />
+                </div>
+                <Card.Body className='mx-4'>
+                    <h4 className="card-title text-center" style={{ color: 'white' }}>
+                        Forgot Password
+                    </h4>
+                    <div className='card-title text-center text-white'>We can help you reset your password using email address associated with your account.</div>
+                    {error && <Alert variant="danger">{error}</Alert>}
+                    <Form className="forgot-form forgot-form-container">
                         {message &&
                             <Alert variant='info'>
                                 {message}
                             </Alert>}
-                        <Form.Group className="mb-3">
+                        <Form.Group className="">
                             <Form.Control
                                 type="text"
-                                placeholder="Username"
-                                value={username}
-                                onChange={(e) => setUsername(e.target.value)}
-                                className="mb-2"
-                                style={{
-                                    backgroundColor: 'rgba(58, 62, 82, 1)',
-                                    color: 'rgba(153, 155, 170, 1)',
-                                }}
-                            />
-                        </Form.Group>
-                        <Form.Group className="mb-3">
-                            <Form.Control
-                                type="password"
-                                placeholder="New Password"
-                                value={newPassword}
-                                onChange={(e) => setNewPassword(e.target.value)}
-                                className="mb-2"
+                                placeholder="Email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                className="my-3"
                                 style={{
                                     backgroundColor: 'rgba(58, 62, 82, 1)',
                                     color: 'rgba(153, 155, 170, 1)',
@@ -63,8 +60,13 @@ const Forgot = () => {
                         </Button>
                     </Form>
                 </Card.Body>
+                <Card.Footer className='text-primary w-100 ' style={{backgroundColor: '#3a3e52',}}>
+                    <a href="/" class="text-reset">Login</a>
+                </Card.Footer>
             </Card>
-        </div>
+        </Container>
+
+        
     );
 };
 
