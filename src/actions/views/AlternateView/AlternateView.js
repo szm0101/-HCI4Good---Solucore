3574e7b6c3f80e25fc02b3a77a63fbf04d83e4c6
Tahseen Shanteer <th362212@dal.ca>
2023-11-15 19:35:22 -0400

    Created building dropdown, still need to configure styling and match buildinhgs with devices

--- src/actions/views/AlternateView/AlternateView.js
+++ src/actions/views/AlternateView/AlternateView.js
@@ -14,7 +14,8 @@ import Devices from "./Devices";
 
 function AlternateView() {
   const [isOpen, setIsOpen] = useState(Array(7).fill(false));
-  const [deviceIds, setDeviceIds] = useState([]);
+  const [deviceIds, setDeviceIds] = useState(null);
+  const [buildings, setBuildings] = useState([]);
 
   const toggle = (index) => {
     setIsOpen((prevIsOpen) =>
@@ -32,23 +33,24 @@ function AlternateView() {
       method: "get",
       headers: header,
     };
-    let deviceIds = [];
-    const fetchDeviceIds = () => {
-      fetch(
-        "https://services.solucore.com/solutrak/api/buildings/getDeviceInfos",
-        requestOptions
-      )
-        .then((response) => response.json())
-        .then((json) => {
-          json.Data.map(
-            (deviceInfo, idx) => (deviceIds[idx] = deviceInfo.deviceId)
-          );
-        })
-        .catch((error) => console.log("API error" + error));
-    };
 
-    setDeviceIds(deviceIds);
-    fetchDeviceIds();
+    let buildings = [];
+    const fetchBuildings = () => {
+      fetch(`https://services.solucore.com/solutrak/api/buildings/getBuildingInfos`,
+      requestOptions
+      )
+      .then((response) => response.json())
+      .then((json) => {
+        json.Data.map(
+          (buildingInfo, idx) => (buildings[idx] = buildingInfo)
+        );
+      })
+      .catch((error) => console.log("API Error" + error));
+    }
+
+    fetchBuildings();
+    setBuildings(buildings);
+
   }, [cookies.token]);
 
   return (
@@ -59,16 +61,12 @@ function AlternateView() {
       <div className="content">
         <table className="main-table">
           <tbody>
-            <tr className="data-tables" onClick={() => toggle(5)}>
-              <td>Building Details</td>
-            </tr>
-            {isOpen[5] ? (
-              <tr>
-                <table className="general-op-data">
-                  <tbody>{<BuildingInfo />}</tbody>
+              <tr className="building-select">
+                <table>
+                  <tbody>{<BuildingInfo buildings={buildings}/>}</tbody>
                 </table>
               </tr>
-            ) : null}
+  
             <tr className="data-tables" onClick={() => toggle(6)}>
               <td>Devices</td>
             </tr>
@@ -82,7 +80,7 @@ function AlternateView() {
             <tr className="data-tables" onClick={() => toggle(0)}>
               <td>General Operating Data</td>
             </tr>
-            {isOpen[0] ? (
+            {isOpen[0] && deviceIds? (
               <tr>
                 <table className="general-op-data">
                 {deviceIds.map((deviceId) => (
@@ -98,7 +96,7 @@ function AlternateView() {
             <tr className="data-tables" onClick={() => toggle(1)}>
               <td>Performance Data</td>
             </tr>
-            {isOpen[1] ? (
+            {isOpen[1] && deviceIds ? (
               <tr>
                 <table className="general-op-data">
                       <tbody>
@@ -110,7 +108,7 @@ function AlternateView() {
             <tr className="data-tables" onClick={() => toggle(2)}>
               <td>Maintenance information</td>
             </tr>
-            {isOpen[2] ? (
+            {isOpen[2] && deviceIds? (
               <tr>
                 <table className="general-op-data">
                   <tbody>{<MaintenanceInfo deviceId={deviceIds[0]}/>}</tbody>
@@ -120,7 +118,7 @@ function AlternateView() {
             <tr className="data-tables" onClick={() => toggle(3)}>
               <td>Fault information</td>
             </tr>
-            {isOpen[3] ? (
+            {isOpen[3] && deviceIds ? (
               <tr>
                 <table className="fault-info-table">
                     {deviceIds.map((deviceId) => (
@@ -135,7 +133,7 @@ function AlternateView() {
             <tr className="data-tables" onClick={() => toggle(4)}>
               <td>Traffic information</td>
             </tr>
-            {isOpen[4] ? (
+            {isOpen[4] && deviceIds ? (
               <tr>
                 <table className="general-op-data">
                   <tbody>
--- src/actions/views/AlternateView/BuildingInfo.js
+++ src/actions/views/AlternateView/BuildingInfo.js
@@ -1,14 +1,52 @@
 import React from "react";
-import './Data.css';
+import "./Data.css";
+import { useState, useEffect } from "react";
+import { useRef } from "react";
+import { DropdownButton, Dropdown } from "react-bootstrap";
 
-function BuildingInfo() {
+function BuildingInfo(props) {
+  const [building, setBuilding] = useState("-- Select Building --");
+  const [dropDownOpen, setDropDownOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const handleToggle = () => {
+    setDropDownOpen(!dropDownOpen);
+  };
+
+  useEffect(() => {
+    if (dropDownOpen && dropdownRef.current) {
+      const dropdownMenu = dropdownRef.current.querySelector(".dropdown-menu");
+      if (dropdownMenu) {
+        const dropdownMenuHeight = dropdownMenu.clientHeight;
+        dropdownRef.current.style.height = `${dropdownMenuHeight}px`;
+      }
+    } else {
+      dropdownRef.current.style.height = "auto";
+    }
+  }, [dropDownOpen]);
+
   return (
-    <body>
-        <div class="buildingInfo">
-            <h4> Building Name</h4>
-            <p> # of devices - company name</p>
+    <div class="buildingInfo" ref={dropdownRef}>
+      <Dropdown show={dropDownOpen} onToggle={handleToggle}>
+        <Dropdown.Toggle id="dropdown-basic">
+          {building}
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          {props.buildings.map((building, idx) => (
+            <Dropdown.Item onClick={() => setBuilding(building)}>
+              {building.buildingName}
+            </Dropdown.Item>
+          ))}
+        </Dropdown.Menu>
+      </Dropdown>
+
+      {building !== "-- Select Building --" ? (
+        <div>
+          <h4>{building.buildingName}</h4>
+          <h4>{`${building.bankInfos[0].deviceInfos.length} - Devices`}</h4>
         </div>
-    </body>
+      ) : null}
+    </div>
   );
 }
 
