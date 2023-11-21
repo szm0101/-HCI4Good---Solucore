1e5073baf5087b854847d9430f10af42007da541
eltazy <mh70000@dal.ca>
2023-11-21 17:22:14 -0400

    Frontend matched to old website, mostly done. Missing edit profile modal.

--- src/actions/views/Profile/Profile.js
+++ src/actions/views/Profile/Profile.js
@@ -1,7 +1,8 @@
 import React, { useState} from 'react';
-import { Card, Form } from 'react-bootstrap';
+import { Card, Form, Button } from 'react-bootstrap';
 import Photo from '../../assets/profile-icon.png'
 import './Profile.css';
+import ChangePasswordModal from '../../components/ChangePasswordModal/Modal.js';
 
 const Profile = () => {
 
@@ -9,37 +10,57 @@ const Profile = () => {
         firstName: 'John',
         lastName: 'Doe',
         phoneNumber: '123-456-7890',
+        mobile: '1',
       });
 
       const [profilePicture, setProfilePicture] = useState(Photo);
     
+      const [passwordModalShow, setPasswordModalShow] = React.useState(false);
+      const [editModalShow, setEditModalShow] = React.useState(false);
+
+      const handlePasswordClose = () => {
+        setPasswordModalShow(false);
+    };
+    const handleEditClose = () => {
+        setEditModalShow(false);
+    };
       
 
     return(
-        <div className=" container mt-4">
+        <div className=" container mt-4 ms-4">
       <div className="row">
         <div className="col-md-6">
-          <Card className='h-100'>
-            <Card.Body>
-              <Card.Title>User Information</Card.Title>
-              <Card.Text>
-                <strong>First Name:</strong> {userData.firstName} <br />
-                <strong>Last Name:</strong> {userData.lastName} <br />
-                <strong>Phone Number:</strong> {userData.phoneNumber} <br />
+          <Card className='h-100 dark-bg text-white'>
+            <Card.Body >
+              <Card.Title className='mt-2 mb-5 fs-2'>User Information</Card.Title>
+              <hr/>
+              <Card.Text className='fs-5 text-start'>
+                <p className='ms-3'>First Name: {userData.firstName} </p> <hr />
+                <p className='ms-3'>Last Name: {userData.lastName} </p> <hr />
+                <p className='ms-3'>Phone Number: {userData.phoneNumber} </p> <hr />
+                <p className='ms-3'>Mobile: {userData.mobile} </p> <hr />
+                <div className="d-flex justify-content-center mt-4">
+                    <Button className='mx-2 btn-lg' onClick={() => setEditModalShow(true)}>
+                        Edit Profile
+                    </Button>
+                    <Button className='mx-2 btn-lg' onClick={() => setPasswordModalShow(true)}>
+                        Change Password
+                    </Button>
+                </div>
               </Card.Text>
             </Card.Body>
           </Card>
         </div>
-        <div className="col-md-6">
-          <Card className='h-100'>
-            <Card.Body className="d-flex flex-column align-items-center justify-content-center">
-              <Card.Title>Profile Picture</Card.Title>
+        <div className="col-md-4">
+          <Card className='h-100 dark-bg text-white'>
+            <Card.Body className="d-flex flex-column align-items-center justify-content-center mb-5">
+              <Card.Title className='mt-2 mb-5 fs-2'>Profile Picture</Card.Title>
               {/* Display the profile picture if available */}
               {profilePicture ? (
                 <img
                   src={Photo}
                   alt="Profile"
-                  className="img-fluid"
+                  className="img-fluid photo"
                 />
               ) : (
                 <p className="text-muted">No picture uploaded</p>
@@ -47,15 +68,19 @@ const Profile = () => {
 
               {/* Upload Button and Form */}
               <Form>
-              <Form.Group controlId="formFile" className="mb-3">
-                <Form.Label>Default file input example</Form.Label>
-                <Form.Control type="file" />
+              <Form.Group controlId="formFile" className="mt-5">
+                <Form.Label className='btn btn-lg btn-primary'>Change Picture</Form.Label>
+                <Form.Control 
+                    type="file"
+                    className='d-none'
+                />
             </Form.Group>
               </Form>
             </Card.Body>
           </Card>
         </div>
         </div>
+        <ChangePasswordModal show={passwordModalShow} onHide={handlePasswordClose} />
         </div>
         );
 };
