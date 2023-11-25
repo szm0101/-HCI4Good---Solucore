1c3efa91a6c9cc58fe35df01c8d3c17d906e784e
eltazy <mh70000@dal.ca>
2023-11-25 14:25:08 -0400

    Styled Banks page to match old website

--- src/actions/views/Buildings/Banks.js
+++ src/actions/views/Buildings/Banks.js
@@ -1,41 +1,50 @@
 import React from "react";
-import { Table } from "react-bootstrap";
-import { useLocation } from "react-router-dom";
+import { Table, Card, Button } from "react-bootstrap";
+import { useLocation, Link } from "react-router-dom";
 
 const Banks = () => {
-  // Sample data for the table
-  
-    const location = useLocation();
-    const banksData = location.state?.bankInfos || [];
-    console.log(banksData);
+  const location = useLocation();
+  const banksData = location.state?.bankInfos || [];
 
   return (
     <div className="buildings-container">
-    <div className="buildings-table">
- 
-      <Table className='table'>
-      <thead>
-      <div className='text-white p-4'>Banks</div>
-        <tr>
-        <th>ID</th>
-            <th>Name</th>
-            <th>Location</th>
-          <th></th>
-          <th></th>
-          <th></th>
-        </tr>
-      </thead>
 
-      <tbody >
+    
+    <Card className="banks-card mt-5" >
+      <Card.Header>
+      <div className="d-flex justify-content-start align-items-center">
+    <div>
+      <Button as={Link} to="/Buildings" variant="secondary">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-left" viewBox="0 0 16 16">
+          <path fillRule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
+        </svg>
+      </Button>
+    </div>
+    <div className="ms-3">
+      <h5 className="mb-0 text-white fw-bold fs-3">Banks</h5>
+    </div>
+  </div>
+      </Card.Header>
+      <Card.Body className="buildings-table px-0 py-0">
+        <Table className="table-dark">
+          <thead>
+            <tr >
+              <th className="text-start text-white-50 fw-bold fs-5 ps-5">Bank Name</th>
+              <th className="text-start text-white-50 fw-bold fs-5 ps-5">ID</th>
+              <th></th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
             {banksData ? (
               banksData.map((bank) => (
                 <tr key={bank.id}>
-                  <td className='text-white'>{bank.bankId}</td>
-                 <td className='text-white'>{bank.bankName}</td>
-                 <td className='text-white'>{bank.location}</td>
-                  <td className='text-white'>View</td>
-                  <td className='text-white'>Edit</td>
-                  <td className='text-white'>Delete</td>
+                    <td className="text-white-50 fw-bold fs-5 ps-5">{bank.bankName}</td>
+                  <td className="text-white-50 fw-bold fs-5 ps-5">{bank.bankId}</td>
+                  <td className="text-white">View</td>
+                  <td className="text-white">Edit</td>
+                  <td className="text-white">Delete</td>
                 </tr>
               ))
             ) : (
@@ -43,10 +52,11 @@ const Banks = () => {
                 <td colSpan="6">Loading...</td>
               </tr>
             )}
-      </tbody>
-      </Table>
+          </tbody>
+        </Table>
+      </Card.Body>
+    </Card>
     </div>
-  </div>
   );
 };
 
