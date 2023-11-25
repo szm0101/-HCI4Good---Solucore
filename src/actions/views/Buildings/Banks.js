94e813a7d3d3eec658c632473a57e32de79a4555
TheRealSlimTazy <tazy0002@gmail.com>
2023-11-25 00:42:09 -0400

    Made banks table style match buildings table style

--- src/actions/views/Buildings/Banks.js
+++ src/actions/views/Buildings/Banks.js
@@ -11,27 +11,43 @@ const Banks = () => {
   ];
 
   return (
-    <div className="container mt-4">
-      <h2>Banks</h2>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>ID</th>
+    <div className="buildings-container">
+    <div className="buildings-table">
+ 
+      <Table className='table'>
+      <thead>
+      <div className='text-white p-4'>Banks</div>
+        <tr>
+        <th>ID</th>
             <th>Name</th>
             <th>Location</th>
-          </tr>
-        </thead>
-        <tbody>
-          {banksData.map((bank) => (
-            <tr key={bank.id}>
-              <td>{bank.id}</td>
-              <td>{bank.name}</td>
-              <td>{bank.location}</td>
-            </tr>
-          ))}
-        </tbody>
+          <th></th>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+
+      <tbody >
+            {banksData ? (
+              banksData.map((bank) => (
+                <tr key={bank.id}>
+                  <td className='text-white'>{bank.id}</td>
+                 <td className='text-white'>{bank.name}</td>
+                 <td className='text-white'>{bank.location}</td>
+                  <td className='text-white'>View</td>
+                  <td className='text-white'>Edit</td>
+                  <td className='text-white'>Delete</td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="6">Loading...</td>
+              </tr>
+            )}
+      </tbody>
       </Table>
     </div>
+  </div>
   );
 };
 
